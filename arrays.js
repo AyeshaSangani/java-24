9ac69ba7075beb812ebd54845e4329ecacbe68b5
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2024-12-03 23:53:46 +0500

    ARRAYS

---
+++ arrays.js
@@ -0,0 +1,37 @@
+
+
+            // ============ARRAYS -------store collection of data
+
+let userData =[
+   {name: "ahmed",
+    email: "ahmed@gmail.com",
+    status: "web designer"
+   },
+    
+   {name: "hammad",
+    email: "hammad@gmail.com",
+    status: "Mern Stack Developer"
+   },
+
+   {name: "sammiya",
+    email: "sammiya@gmail.com",
+    status: "full Stack Developer"
+   },
+
+   {name: "saad",
+    email: "xyz@gmail.com",
+    status: "Digital Marketer"
+   },
+
+   {name: "abdullah",
+    email: "abc@gmail.com",
+    status: "Graphic designer"
+   },
+
+   {name: "Moosa",
+    email: "info@gmail.com",
+    status: "Content Creator"
+   },
+
+]
+console.log(userData);
